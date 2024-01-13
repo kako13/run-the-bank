cf5eeb18c6385d5d9ead1b5c008773210327b145
Kauê Oliveira <kso13@hotmail.com>
2024-01-13 08:06:20 -0300

    Criação do projeto

---
+++ src/main/java/com/kaue/runthebank/RunTheBankApplication.java
@@ -0,0 +1,13 @@
+package com.kaue.runthebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunTheBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunTheBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaue/runthebank/RunTheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaue.runthebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTheBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

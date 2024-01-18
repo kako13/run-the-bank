4f9d4c693e0523b3455fafbe85b979eb880f7afc
Kauê Oliveira <kso13@hotmail.com>
2024-01-18 06:38:59 -0300

    Incluindo dataCadastro e trabalhando com UTC

--- src/main/java/com/kaue/runthebank/RunTheBankApplication.java
+++ src/main/java/com/kaue/runthebank/RunTheBankApplication.java
@@ -3,10 +3,15 @@ package com.kaue.runthebank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
+
 @SpringBootApplication
 public class RunTheBankApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
 		SpringApplication.run(RunTheBankApplication.class, args);
 	}
 
--- src/main/java/com/kaue/runthebank/adapters/inboud/entity/ClienteEntity.java
+++ src/main/java/com/kaue/runthebank/adapters/inboud/entity/ClienteEntity.java
@@ -2,30 +2,26 @@ package com.kaue.runthebank.adapters.inboud.entity;
 
 import com.kaue.runthebank.application.core.domain.TipoDocumento;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.OffsetDateTime;
 
 @Entity(name = "cliente")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 public class ClienteEntity {
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String endereco;
-    String senha;
-    String documento;
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String senha;
+    private String documento;
     @Enumerated(EnumType.STRING)
-    TipoDocumento tipoDocumento;
+    private TipoDocumento tipoDocumento;
     @CreationTimestamp
     private OffsetDateTime dataCadastro;
 }

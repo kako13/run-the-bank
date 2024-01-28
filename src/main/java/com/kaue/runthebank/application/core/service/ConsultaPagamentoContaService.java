c97561b771c267f1debd50461eb94add48c3d397
Kauê Oliveira <kso13@hotmail.com>
2024-01-28 18:31:12 -0300

    Pagamento - Service use case e repository

--- src/main/java/com/kaue/runthebank/application/core/service/ConsultaPagamentoContaService.java
+++ src/main/java/com/kaue/runthebank/application/core/service/ConsultaPagamentoContaService.java
@@ -8,7 +8,7 @@ import com.kaue.runthebank.application.ports.out.pagamento.ConsultaPagamentoPort
 import java.util.List;
 
 public class ConsultaPagamentoContaService implements ConsultaPagamentoContaServicePort {
-    public static final String PAGAMENTO_NAO_ENCONTRADO = "Não foi encontrado nenhum pagamento de código '%s' " +
+    public static final String MSG_PAGAMENTO_NAO_ENCONTRADO = "Não foi encontrado nenhum pagamento de código '%s' " +
             "realizado por esta conta.";
     private final ConsultaPagamentoPort consultaPagamentoPort;
 
@@ -25,13 +25,13 @@ public class ConsultaPagamentoContaService implements ConsultaPagamentoContaServ
     public Pagamento buscar(String codigoPagamento) {
         return consultaPagamentoPort.buscar(codigoPagamento)
                 .orElseThrow(() -> new NegocioException(String.format(
-                        PAGAMENTO_NAO_ENCONTRADO, codigoPagamento)));
+                        MSG_PAGAMENTO_NAO_ENCONTRADO, codigoPagamento)));
     }
 
     @Override
     public Pagamento buscarPorCodigoEConta(String codigoPagamento, Long contaId) {
         return consultaPagamentoPort.buscarPorCodigoEConta(codigoPagamento, contaId)
                 .orElseThrow(() -> new NegocioException(String.format(
-                        PAGAMENTO_NAO_ENCONTRADO, codigoPagamento)));
+                        MSG_PAGAMENTO_NAO_ENCONTRADO, codigoPagamento)));
     }
 }
